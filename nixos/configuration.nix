f4761059da9ae0c0fa1fa64c70faf8f84f06a754
SKR <sascha@krusbersky.net>
2024-04-02 13:56:12 +0200

    modify conf

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,6 +2,8 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
+   ./disko-config.nix
   ];
 
   boot.loader.systemd-boot.enable = true; # (for UEFI systems only)
